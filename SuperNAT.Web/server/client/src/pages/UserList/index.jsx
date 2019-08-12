1802f7a6a420673183d492877f5da78b9cf9a0b3
yangan <1305336099@qq.com>
2019-08-13 00:47:47 +0800

    用户管理模块

--- SuperNAT.Web/server/client/src/pages/UserList/index.jsx
+++ SuperNAT.Web/server/client/src/pages/UserList/index.jsx
@@ -3,35 +3,41 @@ import TopBar from '@/components/TopBar';
 import GeneralDialog from '@/components/GeneralDialog';
 import UsersTable from './components/UsersTable';
 import DataBinder from '@icedesign/data-binder';
+import request from '@/utils/request'
 
-export default function UserList() {
-  DataBinder({
-    userData: {
-      url: '/Api/User/GetList',
-      method: 'POST',
-      responseFormatter: (responseHandler, body, response) => {
-        // 拿到接口返回的 res 数据，做一些格式转换处理，使其符合 DataBinder 的要求
-        console.log('body', body)
-        responseHandler(body, response)
-      },
-      defaultBindingData: {
-        tableData: []
-      }
+@DataBinder({
+  userData: {
+    url: '/Api/User/GetList',
+    method: 'POST',
+    data: {},
+    defaultBindingData: {
+      dataSource: []
     }
-  })
-  // const { userData } = this.DataBinder
-  // const tableData = userData.tableData
-  // const getFormValue = (value) => {
-  //   tableData.push(value)
-  // };
-  const tableData = []
-  return (
-    <div>
-      <TopBar
-        title="用户管理"
-        extraAfter={<GeneralDialog buttonText="新建用户" />}
-      />
-      <UsersTable data={tableData} />
-    </div>
-  );
+  }
+}, { requestClient: request })
+export default class UserList extends Component {
+  componentDidMount() {
+    // 组件加载时获取数据源，数据获取完成会触发组件 render
+    this.props.updateBindingData('userData', {
+      data: {
+        user_name: '123'
+      }
+    });
+  }
+  render() {
+    const { userData } = this.props.bindingData
+    const tableData = userData.dataSource
+    const getFormValue = (value) => {
+      tableData.push(value)
+    };
+    return (
+      <div>
+        <TopBar
+          title="用户管理"
+          extraAfter={<GeneralDialog buttonText="新建用户" getFormValue={getFormValue} />}
+        />
+        <UsersTable data={tableData} />
+      </div>
+    );
+  }
 }
--- SuperNAT.Web/server/client/src/utils/request.js
+++ SuperNAT.Web/server/client/src/utils/request.js
@@ -1,22 +1,43 @@
-import axios from 'axios'
-import {
-  getToken
-} from '@/utils/auth'
+import axios from "axios";
+import { getToken } from "@/utils/auth";
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://localhost:8088', // api的base_url
+  baseURL: "http://localhost:8088", // api的base_url
   timeout: -1 // request timeout
-})
+});
 
 // request interceptor
-service.interceptors.request.use(config => {
-  // Do something before request is sent]
-  config.headers['X-Token'] = getToken() // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
-  return config
-}, error => {
-  // Do something with request error
-  Promise.reject(error)
-})
+service.interceptors.request.use(
+  config => {
+    // Do something before request is sent]
+    config.headers["X-Token"] = getToken(); // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
+    return config;
+  },
+  error => {
+    // Do something with request error
+    Promise.reject(error);
+  }
+);
 
-export default service
+service.interceptors.response.use(
+  response => {
+    // 拿到接口返回的 res 数据，做一些格式转换处理，使其符合 DataBinder 的要求
+    const body = response.data;
+    const newBody = {
+      status: body.Result ? "SUCCESS" : "ERROR",
+      message: body.Message,
+      data: {
+        dataSource: body.Data
+      }
+    };
+    response.data = newBody;
+    return response;
+  },
+  error => {
+    console.log("err" + error); // for debug
+    return Promise.reject(error);
+  }
+);
+
+export default service;
