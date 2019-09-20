826e440bf4131354592c7fc8a515ca306cf32d51
yangan <1305336099@qq.com>
2019-09-20 23:50:58 +0800

    优化登录

--- SuperNAT.Web/manage/src/views/auth/Login.vue
+++ SuperNAT.Web/manage/src/views/auth/Login.vue
@@ -17,14 +17,23 @@
                       name="login"
                       :label="$t('login.account')"
                       type="text"
-                      v-model="model.user_name" />
+                      v-model="model.user_name"
+                      v-validate="'required'"
+                      :error-messages="errors.collect('user_name')"
+                      data-vv-name="user_name"
+                      required />
 
         <v-text-field append-icon="lock"
                       name="password"
                       :label="$t('login.password')"
                       id="password"
                       type="password"
-                      v-model="model.password" />
+                      @keyup.enter.native="login"
+                      v-model="model.password"
+                      v-validate="'required'"
+                      :error-messages="errors.collect('password')"
+                      data-vv-name="password"
+                      required />
       </v-form>
     </v-card-text>
 
@@ -63,35 +72,57 @@
 import request from "@/util/request"
 import { setJwtToken } from "@/util/auth";
 export default {
+  $_veeValidate: {
+    validator: 'new'
+  },
   data: () => ({
     loading: false,
     model: {
       user_name: "",
       password: ""
+    },
+    dictionary: {
+      custom: {
+        user_name: {
+          required: () => '账号不能为空'
+        },
+        password: {
+          required: () => '密码不能为空'
+        }
+      }
     }
   }),
+  mounted () {
+    this.$validator.localize('zh', this.dictionary)
+  },
   methods: {
     login () {
       this.loading = true
       // handle login
-      request({
-        url: '/Api/User/Login',
-        method: 'post',
-        data: this.model
-      }).then(({ data }) => {
-        this.loading = false
-        if (data.Result) {
-          this.$dialog.message.success(data.Message, {
-            position: 'top'
+      this.$validator.validateAll(this.formItem).then(res => {
+        if (res) {
+          request({
+            url: '/Api/User/Login',
+            method: 'post',
+            data: this.model
+          }).then(({ data }) => {
+            this.loading = false
+            if (data.Result) {
+              this.$dialog.message.success(data.Message, {
+                position: 'top'
+              })
+              //记录到cookies
+              setJwtToken(data.Data.token)
+              this.$store.dispatch('setUser', data.Data)
+              this.$router.push("/dashboard")
+            } else {
+              this.$dialog.message.error(data.Message, {
+                position: 'top'
+              })
+            }
           })
-          //记录到cookies
-          setJwtToken(data.Data.token)
-          this.$store.dispatch('setUser', data.Data)
-          this.$router.push("/dashboard")
         } else {
-          this.$dialog.message.error(data.Message, {
-            position: 'top'
-          })
+          this.loading = false
         }
       })
     }
