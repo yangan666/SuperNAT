76cb8ecb7cd2b274723b16da331920b9441be8f1
yangan <1305336099@qq.com>
2019-10-18 08:20:32 +0800

    安装、卸载bat脚本

---
+++ SuperNAT.Server/安装.bat
@@ -0,0 +1,16 @@
+@echo off
+if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
+bcdedit >nul
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
+:UACPrompt
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+exit /B
+:UACAdmin
+cd /d "%~dp0"
+
+echo %cd% 
+SuperNAT.Server.exe install
+SuperNAT.Server.exe start
+
+pause
+
