4a79864a29cf2687ee197932c36a07fd61f58cb4
yangan <1305336099@qq.com>
2020-03-09 12:30:30 +0800

    异步优化

--- SuperNAT.Client/Socket/HttpClientProxy.cs
+++ SuperNAT.Client/Socket/HttpClientProxy.cs
@@ -15,107 +15,104 @@ namespace SuperNAT.Client
 {
     public class HttpClientProxy
     {
-        public static void ProcessData(NatClient natClient, NatPackageInfo packageInfo)
+        public static async void ProcessData(NatClient natClient, NatPackageInfo packageInfo)
         {
-            Task.Run(async () =>
+            try
             {
-                try
+                switch (packageInfo.Body.Action)
                 {
-                    switch (packageInfo.Body.Action)
-                    {
-                        case (byte)HttpAction.Request:
-                            {
-                                var httpModel = packageInfo.Body.Data.FromJson<HttpModel>();
-                                var map = ClientHandler.MapList.Find(c => c.remote_endpoint == httpModel.Host || (c.remote == httpModel.Host && c.remote_port == 80));
-                                if (map == null)
-                                {
-                                    HandleLog.WriteLine($"映射不存在，外网访问地址：{httpModel.Host}");
-                                    return;
-                                }
-                                using HttpRequestMessage httpRequest = new HttpRequestMessage()
-                                {
-                                    Method = new HttpMethod(httpModel.Method),
-                                    RequestUri = new Uri($"{map.protocol}://{map.local_endpoint}{httpModel.Path}")
-                                };
-                                HandleLog.WriteLine($"{map.name} {httpModel.Method} {httpRequest.RequestUri.AbsoluteUri} {httpModel.Headers.ToJson()}");
-                                if (httpRequest.Method != HttpMethod.Get && httpModel.Content?.Length > 0)
-                                {
-                                    var body = DataHelper.Decompress(httpModel.Content);//解压
-                                    var bodyStr = body.ToUTF8String();
-                                    //记录请求小于1kb的参数
-                                    if (httpModel.Content.Length < 1024)
-                                    {
-                                        HandleLog.WriteLine($"{map.name} {httpModel.Method} {httpRequest.RequestUri.AbsoluteUri} {bodyStr}");
-                                    }
-                                    httpRequest.Content = new StringContent(bodyStr, Encoding.UTF8, httpModel.ContentType.Split(";")[0]);
-                                }
-                                using HttpClient _httpClient = new HttpClient();
-                                //替换Host 不然400 Bad Request
-                                //httpModel.Headers["Host"] = map.local_endpoint;
-                                foreach (var item in httpModel.Headers)
-                                {
-                                    if (item.Key.ToUpper() != "Content-Type".ToUpper())
-                                    {
-                                        if (!httpRequest.Content?.Headers.TryAddWithoutValidation(item.Key, item.Value) ?? true)
-                                        {
-                                            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation(item.Key, item.Value);
-                                        }
-                                    }
-                                }
-                                if (map.protocol == "https")
-                                {
-                                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-                                }
-                                var response = await _httpClient.SendAsync(httpRequest);
-                                //回传给服务器
-                                httpModel.HttpVersion = $"{map.protocol.ToUpper()}/{response.Version.ToString()}";
-                                httpModel.StatusCode = (int)response.StatusCode;
-                                httpModel.StatusMessage = response.StatusCode.ToString();
-                                httpModel.Local = map.local_endpoint;
-                                httpModel.Headers = response.Headers.ToDictionary();
-                                httpModel.ResponseTime = DateTime.Now;
-                                foreach (var item in response.Content.Headers)
-                                {
-                                    httpModel.Headers.Add(item.Key, string.Join(";", item.Value));
-                                    if (item.Key.ToUpper() == "Content-Type".ToUpper())
-                                    {
-                                        httpModel.ContentType = string.Join(";", item.Value);
-                                    }
-                                }
-                                httpModel.Headers.Remove("Transfer-Encoding");//response收到的是完整的 这个响应头要去掉 不然浏览器解析出错
-                                var returnContent = DataHelper.StreamToBytes(response.Content.ReadAsStreamAsync().Result);
-                                if (returnContent.Length > 0)
-                                    httpModel.Content = DataHelper.Compress(returnContent);
-                                var pack = PackHelper.CreatePack(new JsonData()
-                                {
-                                    Type = (int)JsonType.HTTP,
-                                    Action = (int)HttpAction.Response,
-                                    Data = httpModel.ToJson()
-                                });
-                                natClient?.Send(pack);
-                                HandleLog.WriteLine($"{map.name} {httpModel.Method} {httpRequest.RequestUri.AbsoluteUri}{$" {returnContent.ToUTF8String()}".If(returnContent.Length < 1024)} {httpModel.StatusCode} {httpModel.StatusMessage} {Math.Round(returnContent.Length * 1.00 / 1024, 1)}KB");
-                                break;
-                            }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    HandleLog.WriteLine($"处理请求异常：{ex}");
-
-                    var pack = PackHelper.CreatePack(new JsonData()
-                    {
-                        Type = (int)JsonType.HTTP,
-                        Action = (int)HttpAction.Response,
-                        Data = new HttpModel()
+                    case (byte)HttpAction.Request:
                         {
-                            StatusCode = (int)HttpStatusCode.BadRequest,
-                            ContentType = "text/html",
-                            Content = DataHelper.Compress(Encoding.UTF8.GetBytes($"server error"))
-                        }.ToJson()
-                    });
-                    natClient?.Send(pack);
+                            var httpModel = packageInfo.Body.Data.FromJson<HttpModel>();
+                            var map = ClientHandler.MapList.Find(c => c.remote_endpoint == httpModel.Host || (c.remote == httpModel.Host && c.remote_port == 80));
+                            if (map == null)
+                            {
+                                HandleLog.WriteLine($"映射不存在，外网访问地址：{httpModel.Host}");
+                                return;
+                            }
+                            using HttpRequestMessage httpRequest = new HttpRequestMessage()
+                            {
+                                Method = new HttpMethod(httpModel.Method),
+                                RequestUri = new Uri($"{map.protocol}://{map.local_endpoint}{httpModel.Path}")
+                            };
+                            HandleLog.WriteLine($"{map.name} {httpModel.Method} {httpRequest.RequestUri.AbsoluteUri} {httpModel.Headers.ToJson()}");
+                            if (httpRequest.Method != HttpMethod.Get && httpModel.Content?.Length > 0)
+                            {
+                                var body = DataHelper.Decompress(httpModel.Content);//解压
+                                var bodyStr = body.ToUTF8String();
+                                //记录请求小于1kb的参数
+                                if (httpModel.Content.Length < 1024)
+                                {
+                                    HandleLog.WriteLine($"{map.name} {httpModel.Method} {httpRequest.RequestUri.AbsoluteUri} {bodyStr}");
+                                }
+                                httpRequest.Content = new StringContent(bodyStr, Encoding.UTF8, httpModel.ContentType.Split(";")[0]);
+                            }
+                            using HttpClient _httpClient = new HttpClient();
+                            //替换Host 不然400 Bad Request
+                            //httpModel.Headers["Host"] = map.local_endpoint;
+                            foreach (var item in httpModel.Headers)
+                            {
+                                if (item.Key.ToUpper() != "Content-Type".ToUpper())
+                                {
+                                    if (!httpRequest.Content?.Headers.TryAddWithoutValidation(item.Key, item.Value) ?? true)
+                                    {
+                                        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation(item.Key, item.Value);
+                                    }
+                                }
+                            }
+                            if (map.protocol == "https")
+                            {
+                                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+                            }
+                            var response = await _httpClient.SendAsync(httpRequest);
+                            //回传给服务器
+                            httpModel.HttpVersion = $"{map.protocol.ToUpper()}/{response.Version.ToString()}";
+                            httpModel.StatusCode = (int)response.StatusCode;
+                            httpModel.StatusMessage = response.StatusCode.ToString();
+                            httpModel.Local = map.local_endpoint;
+                            httpModel.Headers = response.Headers.ToDictionary();
+                            httpModel.ResponseTime = DateTime.Now;
+                            foreach (var item in response.Content.Headers)
+                            {
+                                httpModel.Headers.Add(item.Key, string.Join(";", item.Value));
+                                if (item.Key.ToUpper() == "Content-Type".ToUpper())
+                                {
+                                    httpModel.ContentType = string.Join(";", item.Value);
+                                }
+                            }
+                            httpModel.Headers.Remove("Transfer-Encoding");//response收到的是完整的 这个响应头要去掉 不然浏览器解析出错
+                            var returnContent = DataHelper.StreamToBytes(response.Content.ReadAsStreamAsync().Result);
+                            if (returnContent.Length > 0)
+                                httpModel.Content = DataHelper.Compress(returnContent);
+                            var pack = PackHelper.CreatePack(new JsonData()
+                            {
+                                Type = (int)JsonType.HTTP,
+                                Action = (int)HttpAction.Response,
+                                Data = httpModel.ToJson()
+                            });
+                            natClient?.Send(pack);
+                            HandleLog.WriteLine($"{map.name} {httpModel.Method} {httpRequest.RequestUri.AbsoluteUri}{$" {returnContent.ToUTF8String()}".If(returnContent.Length < 1024)} {httpModel.StatusCode} {httpModel.StatusMessage} {Math.Round(returnContent.Length * 1.00 / 1024, 1)}KB");
+                            break;
+                        }
                 }
-            });
+            }
+            catch (Exception ex)
+            {
+                HandleLog.WriteLine($"处理请求异常：{ex}");
+
+                var pack = PackHelper.CreatePack(new JsonData()
+                {
+                    Type = (int)JsonType.HTTP,
+                    Action = (int)HttpAction.Response,
+                    Data = new HttpModel()
+                    {
+                        StatusCode = (int)HttpStatusCode.BadRequest,
+                        ContentType = "text/html",
+                        Content = DataHelper.Compress(Encoding.UTF8.GetBytes($"server error"))
+                    }.ToJson()
+                });
+                natClient?.Send(pack);
+            }
         }
     }
 }
